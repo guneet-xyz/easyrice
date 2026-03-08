628ff6285028af5e31ca2851e07f288d2a216807
Guneet Aggarwal <guneetaggarwal@gmail.com>
2026-03-08 15:33:19 +0530

    style: remove muted background from sign-in page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,9 +1,9 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "./_components/form"
 
-export default function Page() {
+export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center grow">
-      <div className="w-1/2">
+    <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
         <LoginForm />
       </div>
     </div>
